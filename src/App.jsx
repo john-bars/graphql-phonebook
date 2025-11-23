541fe96b13ac8ca1357280fd8769b8d296998765
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-11-23 14:10:41 +0200

    Add part8-1

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import { gql } from '@apollo/client'
+import { useQuery } from '@apollo/client/react'
+import Persons from './components/Persons'
+
+const ALL_PERSONS = gql`
+  query {
+    allPersons {
+      name
+      phone
+      id
+    }
+  }
+`
+
+const App = () => {
+  const result = useQuery(ALL_PERSONS)
+
+  if (result.loading) {
+    return <div>loading...</div>
+  }
+
+  return <Persons persons={result.data.allPersons} />
+}
+
+export default App
