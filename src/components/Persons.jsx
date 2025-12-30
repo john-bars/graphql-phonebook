1b68abf6d1db8f0d442cdde8d515377abdd95b19
Vesa Jolkkonen <vesa.jolkkonen@hotmail.com>
2025-12-30 17:58:46 +0200

    Use id as a key instead of name

--- src/components/Persons.jsx
+++ src/components/Persons.jsx
@@ -49,7 +49,7 @@ const Persons = ({ persons }) => {
     <div>
       <h2>Persons</h2>
       {persons.map((p) => (
-        <div key={p.name}>
+        <div key={p.id}>
           {p.name} {p.phone}
           <button onClick={() => setNameToSearch(p.name)}>show address</button>
         </div>
